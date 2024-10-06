a163cc1cedb431004e3122e554dfe6565b5e6ef9
leonard35200 <146414414+leonard35200@users.noreply.github.com>
2024-10-06 18:49:39 +0200

    Update script.js

--- compteur/script.js
+++ compteur/script.js
@@ -3,21 +3,6 @@ let nomsJoueur = [];
 let nomsJoueurFin = JSON.parse(sessionStorage.getItem('nomsJoueur'));
 let nombreJoueurFin = sessionStorage.getItem('nombreJoueur');
 
-let categoriesSevenWonders = [ 'Pièce', 'Merveille','Armée',' ',' ',' ',' '];
-let couleursSevenWonders = [ 'grey','grey','red', 'blue', 'yellow', 'purple', 'green'];
-
-let categoriesSevenWondersDuel = [" "," ", " "," ", "merveille","scientifique","pièces",""];
-let couleursSevenWondersDuel = [ 'royalblue','green','gold',"#7B1FA2", '#FFECB3',"#2E7D32", "#FDD835","#C62828"];
-
-let categoriesPetitesBourgardes = ["","","","","","","",""]
-let couleursPetitesBourgardes = ["","","","","","","",""]
-let imagesPetitesBourgardes = ["Usine.png","Maison.png","Eglise.png","Taverne.png","jaune.png","puit.png","tour.png","casevide.png"]
-
-
-function test(){
-  console.log('test')
-}
-
 
 function changerNombreJoueur(value) {
   nombreJoueur = value;
@@ -94,135 +79,99 @@ function saveInputValues() {
     nomsJoueur.push(inputValue);
   }
 
-sessionStorage.setItem('nomsJoueur', JSON.stringify(nomsJoueur));
+sessionStorage.setItem('nomsJoueurs', JSON.stringify(nomsJoueur));
   nomsJoueurFin = JSON.parse(sessionStorage.getItem('nomsJoueur'));
   var divJeux = document.getElementById("jeux");
- divJeux.style.display = "block";
+ divJeux.style.display = "block"; 
 }
 
-function updateTableSeven(categorie, couleurs, images) {
+function updateTableSeven() {
 
-    var nomsJoueurs = JSON.parse(sessionStorage.getItem('nomsJoueur'));
-    console.log(nomsJoueurs);
-  
-    // Efface le contenu actuel du tableau
-    var table = document.getElementById('scoreTableSeven');
-    table.innerHTML = '';
 
-    // Génère les colonnes du tableau en fonction du nombre de joueurs
-    var headerRowSeven = table.insertRow(0);
-    headerRowSeven.insertCell(0);
+            // Efface le contenu actuel du tableau
+            var table = document.getElementById('scoreTableSeven');
+            table.innerHTML = '';
 
-    for (var i = 1; i <= nombreJoueurFin; i++) {
-        var cell = headerRowSeven.insertCell(i);
-        cell.innerHTML = nomsJoueurFin[i-1];
-        cell.style.backgroundColor = "BlanchedAlmond";
-    }
+            // Génère les colonnes du tableau en fonction du nombre de joueurs
+            var headerRowSeven= table.insertRow(0);
+            headerRowSeven.insertCell(0);
 
-    // Génère les lignes du tableau pour chaque catégorie avec les images
-    for (var i = 0; i < categorie.length; i++) {
-        var row = table.insertRow(-1);
-        var cell = row.insertCell(0);
+            for (var i = 1; i <= nombreJoueurFin; i++) {
+                var cell = headerRowSeven.insertCell(i);
+                cell.innerHTML = 'Joueur ' + i;
+           cell.style.backgroundColor ="BlanchedAlmond";
+            }
 
-        // Ajouter une image dans la cellule de la première colonne
-        if (images && images[i]) {
-            cell.innerHTML = '<img src="' + images[i] + '" alt="' + categorie[i] + '" style="width:50px;height:50px;">';
-        } else {
-            cell.innerHTML = categorie[i];
+            // Génère les lignes du tableau pour chaque catégorie
+            var categoriesSeven = [ 'Pièce', 'Merveille','Armée',' ',' ',' ',' ']; // Ajoutez d'autres catégories au besoin
+            var couleursSeven = [ 'grey','grey','red', 'blue', 'yellow', 'purple', 'green']; // Ajoutez d'autres couleurs au besoin
+            for (var i = 0; i < categoriesSeven.length; i++) {
+                var row = table.insertRow(-1);
+                var cell = row.insertCell(0);
+                cell.innerHTML = categoriesSeven[i];
+                cell.style.backgroundColor = couleursSeven[i];        
+
+
+                for (var j = 1; j <= nombreJoueurFin; j++) {
+                    var cell = row.insertCell(j);
+                    cell.contentEditable = true;
+                }
+            }
         }
 
-        cell.style.backgroundColor = couleurs[i];
-
-        for (var j = 1; j <= nombreJoueurFin; j++) {
-            var cell = row.insertCell(j);
-            cell.contentEditable = true;
-        }
-    }
-}
 function calculateScoresSeven() {
-    // Logique de calcul des scores ici (omise pour la clartÃ©)
+            // Logique de calcul des: scores ici
 
-    // Exemple simple : addition des points par colonne
-    var totalPointsSeven = [];
-    var table = document.getElementById('scoreTableSeven');
+            // Exemple simple : addition des points par colonne
+            var totalPointsSeven = [];
+            var table = document.getElementById('scoreTableSeven');
 
-    for (var i = 1; i < table.rows[0].cells.length; i++) {
-        var total = 0;
-        for (var j = 1; j < table.rows.length; j++) {
-            total += parseInt(table.rows[j].cells[i].innerText) || 0;
-        }
-        totalPointsSeven.push(total);
-    }
+            for (var i = 1; i < table.rows[0].cells.length; i++) {
+                var total = 0;
+                for (var j = 1; j < table.rows.length; j++) {
+                    total += parseInt(table.rows[j].cells[i].innerText) || 0;
+                }
+                totalPointsSeven.push(total);
+            }
 
-    // Tri du tableau dans l'ordre dÃ©croissant
-    var playerScores = [];
+            // Afficher les scores finaux
 
-    // Remplir le tableau d'objets avec le numÃ©ro du joueur et ses points
-    for (var i = 0; i < totalPointsSeven.length; i++) {
-        playerScores.push({ player: nomsJoueurFin[i], points: totalPointsSeven[i] });
-    }
+var resultString = 'Scores Finaux:\n';
 
-    // Tri du tableau d'objets dans l'ordre dÃ©croissant en fonction des points
-    playerScores.sort(function(a, b) {
-        return b.points - a.points;
-    });
+// Tri du tableau dans l'ordre décroissant
+var playerScores = [];
 
-    // GÃ©nÃ©rer la chaÃ®ne de rÃ©sultats sans les prÃ©noms des joueurs
-    var resultString = 'Scores Finaux:\n';
-
-    for (var i = 0; i < playerScores.length; i++) {
-        resultString += playerScores[i].player + ' : ' + playerScores[i].points + ' points\n';
-    }
-
-    // Afficher l'alerte avec les rÃ©sultats
-    alert(resultString);
+// Remplir le tableau d'objets avec le numéro du joueur et ses points
+for (var i = 0; i < totalPointsSeven.length; i++) {
+    playerScores.push({ player: i + 1, points: totalPointsSeven[i] });
 }
 
+// Tri du tableau d'objets dans l'ordre décroissant en fonction des points
+playerScores.sort(function(a, b) {
+    return b.points - a.points;
+});
+
+// Générer la chaîne de résultats
+var resultString = 'Scores Finaux:\n';
+
+for (var i = 0; i < playerScores.length; i++) {
+    resultString += 'Joueur ' + playerScores[i].player + ' : ' + playerScores[i].points + ' points\n';
+}
+
+alert(resultString);
+
+}
 
 window.addEventListener('load', function() {
     // Vérifier si la page actuelle est "seven-wonders.html"
-    if (window.location.pathname.endsWith("seven-wonders.html") ) {
+    if (window.location.pathname.endsWith("seven-wonders.html")) {
         // Appeler la fonction uniquement pour la page "seven-wonders.html"
-        updateTableSeven(categoriesSevenWonders, couleursSevenWonders);
-    }
-});
-
-window.addEventListener('load', function() {
-    // Vérifier si la page actuelle est "seven-wonders.html"
-    if (window.location.pathname.endsWith("seven-wonders-duel.html") && nombreJoueurFin === '2') {
-        updateTableSeven(categoriesSevenWondersDuel, couleursSevenWondersDuel);
-     
-    }
-});
-
-window.addEventListener('load', function() {
-    // Vérifier si la page actuelle est "seven-wonders.html"
-    if (window.location.pathname.endsWith("seven-wonders-architects.html") ) {
-        updateTableSeven(categoriesSevenWonders, couleursSevenWonders);
+        updateTableSeven();
     }
 });
 
 
 
-window.addEventListener('load', function() {
-     if (window.location.pathname.endsWith("petite-bourgade.html") ) {
-        updateTableSeven(categoriesPetitesBourgades, couleursPetitesBourgades, imagesPetitesBourgades);
-    }
-});
+    // Ajoutez ici le code spécifique à votre fonction pour cette page
 
 
-// Ajoutez cette fonction pour mettre à jour le bouton après le choix du premier joueur
-
-
-// Modifiez la fonction choisirPremierJoueur pour appeler updateBoutonChoixPremierJoueur
-function choisirPremierJoueur() {
-  var nomsJoueurs = JSON.parse(sessionStorage.getItem('nomsJoueur'));
-  var premierJoueurIndex = Math.floor(Math.random() * nomsJoueurs.length);
-  var ordreJoueurs = nomsJoueurs.slice(premierJoueurIndex).concat(nomsJoueurs.slice(0, premierJoueurIndex));
-  sessionStorage.setItem('nomsJoueur', JSON.stringify(ordreJoueurs));
-  console.log("Ordre des joueurs après choix aléatoire du premier joueur :", ordreJoueurs)
-    alert(ordreJoueurs[0] + " est le premier Joueur");
-}
-
-
-Pourquoi le tableau petite bourgade ne charge plus
