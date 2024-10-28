ce4d619ac01cdae43eca8a7324c34e2b824e30e5
leonard35200 <146414414+leonard35200@users.noreply.github.com>
2024-10-28 12:18:54 +0100

    Update script.js

--- compteur/script.js
+++ compteur/script.js
@@ -179,7 +179,7 @@ window.addEventListener('load', function() {
   } else if (pathname.endsWith("seven-wonders-duel.html") && nombreJoueurFin === 2) {
     updateTableSeven(categoriesSevenWondersDuel, couleursSevenWondersDuel);
   }else if (pathname.endsWith("seven-wonders-duel.html") && nombreJoueurFin !== 2) {
-    alert("vous devez être deux joueurs pour jouer a seven wonders duel")
+    alert('Vous devez être deux joueurs pour jouer a seven wonders duel')
   } else if (pathname.endsWith("seven-wonders-architects.html")) {
     updateTableSeven(categoriesSevenWondersArchitechte, couleursSevenWondersArchitechte);
   } else if (pathname.endsWith("petite-bourgade.html")) {
