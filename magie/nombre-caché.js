4d55bd90e47d059692f601e60a01a31e5bb50ac3
leonard35200 <146414414+leonard35200@users.noreply.github.com>
2025-05-06 16:01:50 +0200

    Update nombre-caché.js

--- magie/nombre-caché.js
+++ magie/nombre-caché.js
@@ -1,30 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {window.scrollTo({ top: 0 })});
+
+    document.addEventListener('DOMContentLoaded', function() {window.scrollTo({ top: 0 })});
 
 
-function devinerChiffre() {
-  const res = parseInt(document.getElementById("resultat").value);
-  const chiffre = 9 - (res % 9);
-  document.getElementById("resultat").value = "";
-  document.getElementById("cc").innerHTML = "<p>Le chiffre que tu as rayé est : " + chiffre + "</p> ";
-  document.getElementById("retour").style.margin = "40px auto 0 auto";
-  document.getElementById("retour").style.display = "block";
-}
+    function devinerChiffre() {
+      const res = parseInt(document.getElementById("resultat").value);
+      const chiffre = 9 - (res % 9);
+      document.getElementById("resultat").value = "";
+      document.getElementById("consigne").innerHTML = "<p>Le chiffre que tu as rayé est : " + chiffre + "</p> ";
+      document.getElementById("retour").style.margin = "40px auto 0 auto";
+      document.getElementById("retour").style.display = "block";
+    }
 
-function recommencer() {
-    const button = document.getElementById('retour');
-    button.classList.add('rotate');
-    setTimeout(() => {
-        button.classList.remove('rotate');
-        window.location.href="nombre-caché.html";
-    }, 500);
-}
+    
 
-document.getElementById('retour').addEventListener('click', function(event) {
-    const button = this;
-    button.classList.add('rotate');
-    setTimeout(() => {
-        button.classList.remove('rotate');
-        window.location.href="nombre-caché.html";
-    }, 500);
-});
-
+    document.getElementById('retour').addEventListener('click', function(event) {
+      const img = document.getElementById('tourner');
+  img.classList.add('rotate');
+  setTimeout(() => {
+    img.classList.remove('rotate');
+            window.location.href="nombre-caché.html";
+        }, 500);
+    });
